0599f6c32bd2ff0d5bafc4f17ef29c6c103f89ab
Begona (AA.TT.) <beminfon@teleco.upv.es>
2019-09-18 13:03:24 +0200

    Subido el fichero

---
+++ P1_COMMOV.m
@@ -0,0 +1,8 @@
+%% Álvaro Miró Merino y Begoña Minguez Fons
+
+% En este scrip de Matlab trataremos los datos obtenidos con el NetMonitor
+
+%% Parte de Bego
+
+% En primer lugar cargamos los datos de un kml cualesquiera
+% Previamente se ha pasado a 
